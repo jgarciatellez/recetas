209184d81e69f67f8ff78b2a18f4564d35787c42
francisco.rodrigo <francisco.rodrigo@juntaex.es>
2017-11-13 10:40:01 +0100

    Listado de las recetas

--- index.php
+++ index.php
@@ -9,3 +9,20 @@ $descriptor=mysqli_connect($servidor,$usuario,$pass,$base_datos)
 or die("Imposible conectar");
 ?>
 <h1>Recetas de cocina española</h1>
+
+<?php
+$consulta=mysqli_query($descriptor,"SELECT * FROM recetas");
+$recetas=mysqli_fetch_all($consulta,MYSQLI_ASSOC);
+?>
+<table>
+	<tr>
+		<th>Título</th>
+		<th>Fecha</th>
+	</tr>
+	<?php foreach ($recetas as $receta):?>
+	<tr>
+		<td><?=$receta['titulo']?></td>
+		<td><?=$receta['fecha']?></td>
+	</tr>
+	<?php endforeach;?>		
+</table>
