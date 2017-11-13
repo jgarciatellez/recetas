2a3a848faf30ebc08895d7c84f403c1722066c56
francisco.rodrigo <francisco.rodrigo@juntaex.es>
2017-11-13 11:17:12 +0100

    Metemos las etiquetas html necesaria

--- index.php
+++ index.php
@@ -8,22 +8,32 @@ $base_datos="recetas";
 $descriptor=mysqli_connect($servidor,$usuario,$pass,$base_datos)
 or die("Imposible conectar");
 ?>
-<h1>Recetas de cocina española</h1>
 
-<?php
-// Obtenemos la lista de recetas de la base de datos
-$consulta=mysqli_query($descriptor,"SELECT * FROM recetas");
-$recetas=mysqli_fetch_all($consulta,MYSQLI_ASSOC);
-?>
-<table>
-	<tr>
-		<th>Título</th>
-		<th>Fecha</th>
-	</tr>
-	<?php foreach ($recetas as $receta):?>
-	<tr>
-		<td><?=$receta['titulo']?></td>
-		<td><?=$receta['fecha']?></td>
-	</tr>
-	<?php endforeach;?>		
-</table>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Recetas</title>
+</head>
+<body>
+    <h1>Recetas de cocina española</h1>
+    
+    <?php
+    // Obtenemos la lista de recetas de la base de datos
+    $consulta=mysqli_query($descriptor,"SELECT * FROM recetas");
+    $recetas=mysqli_fetch_all($consulta,MYSQLI_ASSOC);
+    ?>
+    <table>
+    	<tr>
+    		<th>Título</th>
+    		<th>Fecha</th>
+    	</tr>
+    	<?php foreach ($recetas as $receta):?>
+    	<tr>
+    		<td><?=$receta['titulo']?></td>
+    		<td><?=$receta['fecha']?></td>
+    	</tr>
+    	<?php endforeach;?>		
+    </table>
+</body>
+</html>
