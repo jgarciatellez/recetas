f970d8eb6e398e85067de0c779491ac81ab5b593
francisco.rodrigo <francisco.rodrigo@juntaex.es>
2017-11-13 12:20:24 +0100

    Incluidas librerías de bootstrap y algunos estilos

--- index.php
+++ index.php
@@ -13,9 +13,14 @@ or die("Imposible conectar");
 <html lang="es">
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet"
+		href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.js">
+	</script>
 	<title>Recetas</title>
 </head>
 <body>
+	<div class="container-fluid">
     <h1>Recetas de cocina española</h1>
     
     <?php
@@ -23,17 +28,20 @@ or die("Imposible conectar");
     $consulta=mysqli_query($descriptor,"SELECT * FROM recetas");
     $recetas=mysqli_fetch_all($consulta,MYSQLI_ASSOC);
     ?>
-    <table>
+    <table class="table table-striped table-hover">
     	<tr>
     		<th>Título</th>
+    		<th>Descripción</th>
     		<th>Fecha</th>
     	</tr>
     	<?php foreach ($recetas as $receta):?>
     	<tr>
     		<td><?=$receta['titulo']?></td>
+    		<td><?=$receta['descripcion']?></td>
     		<td><?=$receta['fecha']?></td>
     	</tr>
     	<?php endforeach;?>		
     </table>
+    </div>
 </body>
 </html>
