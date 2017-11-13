cc76480c96fb3449273b4870dae335c2384a5016
francisco.rodrigo <francisco.rodrigo@juntaex.es>
2017-11-13 12:50:18 +0100

    Creado el fichero connection

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$servidor="localhost";
+$usuario="alumno";
+$pass="alumno";
+$base_datos="recetas";
+
+// Conexión al servidor de bbdd
+$descriptor=mysqli_connect($servidor,$usuario,$pass,$base_datos)
+or die("Imposible conectar");
+?>
